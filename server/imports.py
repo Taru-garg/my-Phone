73a59d2a54f2323bdf0a0ffbe79ed0eba98de1b0
Taru-garg <tarugarg2@gmail.com>
2020-10-27 22:03:08 +0530

    Phone Working

--- server/imports.py
+++ server/imports.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from functions import *
 from directories import *
 from lists import *
